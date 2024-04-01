05ef9f0cf36b47632ed730c0d38ce065363085a1
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-01 14:03:36 -0400

    ran the stencil build w/ the react OT

---
+++ packages/react-library/dist/types/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export declare const MyComponent: import("react").ForwardRefExoticComponent<Omit<any, "ref"> & import("react").RefAttributes<HTMLMyComponentElement>>;
--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -5,20 +5,12 @@ export const config: Config = {
   namespace: 'stencil-library',
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
       type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-    },
-    {
-      type: 'docs-readme',
     },
     reactOutputTarget({
       componentCorePackage: 'stencil-library',
       proxiesFile: '../react-library/lib/components/stencil-generated/index.ts',
+      includeImportCustomElements: true,
     }),
   ],
   testing: {
