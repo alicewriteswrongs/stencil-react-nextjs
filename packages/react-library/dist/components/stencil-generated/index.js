6cb55429b09984e58b8ef60747e6d67a01dda2d5
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-01 14:24:45 -0400

    what in the heck anyway

---
+++ packages/react-library/dist/components/stencil-generated/index.js
@@ -0,0 +1,4 @@
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElement as defineMyComponent } from 'stencil-library/components/my-component.js';
+export const MyComponent = createReactComponent('my-component', undefined, undefined, defineMyComponent);
+//# sourceMappingURL=index.js.map
