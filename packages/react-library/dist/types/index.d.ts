898e5935e33f07f66a23bdbcdbf36061a15ba610
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-01 14:12:22 -0400

    huh

--- packages/react-library/dist/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const MyComponent: import("react").ForwardRefExoticComponent<Omit<any, "ref"> & import("react").RefAttributes<HTMLMyComponentElement>>;
