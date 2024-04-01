3be83aa137a8cd3dbda4279c091da6fe4e0ff75d
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-01 13:51:09 -0400

    add react OT to config

--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -15,10 +16,10 @@ export const config: Config = {
     {
       type: 'docs-readme',
     },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+    reactOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../react-library/lib/components/stencil-generated/index.ts',
+    }),
   ],
   testing: {
     browserHeadless: "new",
