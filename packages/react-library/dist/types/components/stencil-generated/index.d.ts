5ae2e37d0e4145a4e80507ddb109ff18d89dde82
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-02 09:26:08 -0400

    add a prettier command to the react library

--- packages/react-library/dist/types/components/stencil-generated/index.d.ts
+++ packages/react-library/dist/types/components/stencil-generated/index.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="react" />
-import type { JSX } from 'stencil-library';
-export declare const MyComponent: import("react").ForwardRefExoticComponent<JSX.MyComponent & Omit<import("react").HTMLAttributes<HTMLMyComponentElement>, "style"> & import("./react-component-lib/interfaces").StyleReactProps & import("react").RefAttributes<HTMLMyComponentElement>>;
+import type { JSX } from "stencil-library";
+export declare const MyComponent: import("react").ForwardRefExoticComponent<
+  JSX.MyComponent &
+    Omit<import("react").HTMLAttributes<HTMLMyComponentElement>, "style"> &
+    import("./react-component-lib/interfaces").StyleReactProps &
+    import("react").RefAttributes<HTMLMyComponentElement>
+>;
--- packages/react-library/lib/components/stencil-generated/index.ts
+++ packages/react-library/lib/components/stencil-generated/index.ts
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from './react-component-lib';
+import { createReactComponent } from "./react-component-lib";
 
-import type { JSX } from 'stencil-library';
+import type { JSX } from "stencil-library";
 
-
-
-export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const MyComponent = /*@__PURE__*/ createReactComponent<
+  JSX.MyComponent,
+  HTMLMyComponentElement
+>("my-component");
