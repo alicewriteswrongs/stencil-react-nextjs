68c66d191eff6bc8f352f80afdadeabeefb4d666
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-02 09:22:03 -0400

    add a prettier command to next-app

--- packages/next-app/src/app/page.tsx
+++ packages/next-app/src/app/page.tsx
@@ -1,11 +1,9 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { MyComponent, defineCustomElements } from "react-library";
 
 defineCustomElements();
 
 export default function Home() {
-  return (
-    <MyComponent first="Stencil, visiting NextJS!" />
-  );
+  return <MyComponent first="Stencil, visiting NextJS!" />;
 }
