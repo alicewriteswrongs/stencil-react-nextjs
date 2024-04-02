241dba045cba095d5df83dac8da193926b900a82
Alice Pote <alice.writes.wrongs@gmail.com>
2024-04-02 10:18:21 -0400

    I copied in the createReactComponent util from the normal React OT

--- packages/react-helpers/src/index.ts
+++ packages/react-helpers/src/index.ts
@@ -1,7 +1,13 @@
-export function htmlToReactJSX() {
+/**
+ * Given a snippet of HTML return a tree of calls to the React `h` function (what React JSX transpiles to).
+ * This can be used to dynamically create JSX at runtime.
+ */
+export function htmlToReactJSX(html: string) {
   return "test";
 }
 
 export function reactPropsToStencilHTML() {
   return "test";
 }
+
+export { createReactComponent } from "./react-component-lib";
